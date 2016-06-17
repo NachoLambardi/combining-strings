bf3799c931a3cd834421181d463234ada467c6d5
Nacho Lambardi <ignaciolambardi@gmail.com>
2016-06-17 19:08:46 -0300

    Use the Lower and Upper cases methods

---
+++ methodslowerupcase.js
@@ -0,0 +1,8 @@
+var name = "Peter Parker";
+console.log(name.length);
+console.log(name.toLocaleLowerCase());
+console.log(name.toLocaleUpperCase());
+console.log(name);
+// In line 3 we get the lower case method.
+// In line 4 we get the upper case method.
+// In line 5 we get the original content of the "name" variable
